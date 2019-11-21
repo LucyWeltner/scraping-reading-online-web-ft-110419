7104c28e6b390080698109af9315b2b97660e401
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-21 19:45:11 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,6 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 change_things = doc.css(".headline-26OIBN").text
 
-course_names = doc.css("title-oE5vT4").text
+course_names = doc.css("title-oE5vT4")
 
 p course_names
